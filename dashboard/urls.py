0fbcf1082f9990f1eebed4a9ec61337e80ff9493
edgar <edgar@learningdata.ie>
2016-02-04 17:48:20 +0000

    Added classes and notices

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView, classes, notices
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='dashboard-index'),
-    url(r'^dashboard$', IndexView.as_view(), name='dashboard-index2')
+    url(r'^dashboard$', IndexView.as_view(), name='dashboard-index'),
+    url(r'^service/classes/$', classes, name='dashboard-classes'),
+    url(r'^service/notices/$', notices, name='dashboard-notices')
 ]
