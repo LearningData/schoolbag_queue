986267d7141cfaf3fd1f6e76bfc57299e2396855
edgar <edgar@learningdata.ie>
2016-02-05 11:19:29 +0000

    added calendar to urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import IndexView, classes, notices
+from .views import IndexView, classes, notices, calendar
 
 urlpatterns = [
     url(r'^dashboard$', IndexView.as_view(), name='dashboard-index'),
     url(r'^service/classes/$', classes, name='dashboard-classes'),
-    url(r'^service/notices/$', notices, name='dashboard-notices')
+    url(r'^service/notices/$', notices, name='dashboard-notices'),
+    url(r'^service/calendar/$', calendar, name='dashboard-calendar')
 ]
