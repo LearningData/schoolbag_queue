e8ffb32b16662e5cf09fc638fd24fd5f69e7c21f
Edgar <edgar@me@edgarlatorre.com.br>
2016-02-06 14:13:16 +0000

    add homeworks to urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import IndexView, classes, notices, calendar
+from .views import IndexView, classes, notices, calendar, homeworks
 
 urlpatterns = [
     url(r'^dashboard$', IndexView.as_view(), name='dashboard-index'),
     url(r'^service/classes/$', classes, name='dashboard-classes'),
     url(r'^service/notices/$', notices, name='dashboard-notices'),
-    url(r'^service/calendar/$', calendar, name='dashboard-calendar')
+    url(r'^service/calendar/$', calendar, name='dashboard-calendar'),
+    url(r'^service/homeworks/$', homeworks, name='dashboard-homeworks')
 ]
